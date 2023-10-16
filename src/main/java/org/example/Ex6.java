741fb4b885cf7093b3e3762d47c54a1a3c7129ee
Ricardo Soares <soaresricaventura@gmail.com>
2023-10-16 14:25:01 +0100

    Ex 6 - Updated, Return the average between the digits

--- src/main/java/org/example/Ex6.java
+++ src/main/java/org/example/Ex6.java
@@ -5,14 +5,16 @@ public class Ex6 {
 
     public int digitSum(int num) {
         int sum = 0;
-
+        int numberOfDigits = 0;
 
         while (num > 0) {
+
             int digit = num % 10;
+            numberOfDigits ++;
             sum += digit;
             num /= 10;
         }
 
-        return sum;
+        return sum/numberOfDigits;
     }
 }
--- src/test/java/Ex6Test.java
+++ src/test/java/Ex6Test.java
@@ -16,8 +16,8 @@ public class Ex6Test {
 
     @Test
     public void digitSumTest(){
-        int expectedValue = 6;
-        int actualValue = ex6.digitSum(1212);
+        int expectedValue = 2;
+        int actualValue = ex6.digitSum(2222);
 
         assertEquals(expectedValue,actualValue,"Digit Sum test failed");
     }
